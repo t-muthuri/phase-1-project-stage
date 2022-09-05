f4c9aef15af2bf18d338856bab0670a1f0ec4cfa
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-09-05 09:11:58 +0300

    Interactive icons added

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const seatNumber = document.getElementById('seat-number');
 const totalAmount = document.getElementById('total-amount');
 const busChoice = document.getElementById('PSV');
 
+// serverStore();
 let amountCharged = +busChoice.value;
 //console.log (amountCharged);
 
@@ -21,18 +22,51 @@ vehicle.addEventListener ('click', (event) => {
 function tailorFeeToSeats (){
     const seatsPicked = document.querySelectorAll('.row .seat.picked');
     // console.log(seatsPicked)
+    // const seatInd = [...seatsPicked].map(function(seat){
+    //     return [...seat].indexOf(seat);
+    // })
+
+    // localStorage.setItem('seatsPicked', JSON.stringify(seatInd));
+
     const numberOfSeatsPicked = seatsPicked.length;
 
     seatNumber.innerText = numberOfSeatsPicked;
     totalAmount.innerText = numberOfSeatsPicked * amountCharged;
 }
 
+
 //pick a bus, book a seat(s) and see the charges
-busChoice.addEventListener ('change', (e)=> {
-    amountCharged = +e.target.value;
+busChoice.addEventListener ('change', (event)=> {
+    amountCharged = +event.target.value;
+    // bookStore (event.target.selectedIndex, event.target.value)
+    //pun intended
     tailorFeeToSeats();
 });
 
+//load ind and value
+function loadDataValue (ind, val){
+    localStorage.setItem('seatInd', ind) 
+    localStorage.setItem('seatVal',val)
+}
+
+// function serverStore(){
+//     const seatsPicked = JSON.parse(localStorage.getItem(seatsPicked))
+
+//     if (seatsPicked !== null && seatsPicked.length > 0){
+//         seats.forEach((seat, index)=> {
+//             if(seatsPicked.indexOf(index)> -1){
+//                 seat.classList.add('picked');
+//             }
+//         })
+//     }
+//     const seatPickedInd =localStorage.getItem('seatPickedInd')
+
+//     if (seatPickedInd != null){
+//         busChoice.selectedIndex = seatInd;
+//     }
+// }
+
+tailorFeeToSeats()
 /*
 PSEUDOCODE
 Use a public API
@@ -44,12 +78,25 @@ Use 3 event listeners
 ..like a psv sacco
 ..like counts increase when the icon is clicked
 ..pick a bus and book a seat
--Onchange
+-Change
 ..colour of the seat changes
 ..amount on the screen updates when a seat is booked
 -DOMContentLoaded
 -Hover
 ..name appears when you hover on a matatu image
 -Keypress
+
+#THE STORY YOU ARE TELLING
+Cyrella, a young lady working at Optica along Moi Avenue is winding up her work day at 5.14 a.m.
+She checks the time on her phone and navigates to the #STAGE app to see if she can get a bus to Thika.
+Sighing, she scrolls down to the booking section and taps the #select-your-PSV option and chooses Super Metro.
+Surprisingly, the next available bus to Thika is just ... 5 minutes away.
+Fortunately, despite it being rush hour, there is a Super Metro bus stuck in traffic along Tom Mboya that is expected at Archives in less than 5 minutes.
+Knowing how long the online queue could get in less than 15 minutes, she promptly picks a window seat and sets about packing her bags to head home.
+In a little over 7 minutes the bus is completely booked and Cyrella walks to the stage.
+She silently marvels at how much has changed since the psv saccos started using the #STAGE app.
+No more standing in line.
+No more haggling for a seat. 
+No more commotion nor congestion at stage.
 
  */
